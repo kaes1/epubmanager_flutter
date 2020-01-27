1a6d1af947477d5dcd6d1a44886a725c8ae20d6c
ksobocik <ksobocik@future-processing.com>
2020-01-27 10:32:36 +0100

    Added status pretty display and BookDetails scrolling.

--- lib/book/BookListService.dart
+++ lib/book/BookListService.dart
@@ -1,38 +1,35 @@
+import 'package:epubmanager_flutter/ApiEndpoints.dart';
 import 'package:epubmanager_flutter/ApiService.dart';
 import 'package:epubmanager_flutter/model/BookListEdit.dart';
 import 'package:epubmanager_flutter/model/BookListEntry.dart';
+import 'package:epubmanager_flutter/model/Status.dart';
+import 'package:flutter/foundation.dart';
 import 'package:get_it/get_it.dart';
-import 'package:epubmanager_flutter/ApiEndpoints.dart';
 import 'package:http/http.dart';
 
 class BookListService {
   ApiService apiService = GetIt.instance.get<ApiService>();
 
-
-
-
   Future<BookListEntry> getBookListEntry(int bookId) {
     return apiService.get(ApiEndpoints.bookList).then((response) {
-      return BookListEntry.listFromJson(
-          response).firstWhere((entry) => entry.book.id == bookId, orElse: () => null);
+      return BookListEntry.listFromJson(response)
+          .firstWhere((entry) => entry.book.id == bookId, orElse: () => null);
     });
   }
-
 
   Future<List<BookListEntry>> getBookList() {
     return apiService.get(ApiEndpoints.bookList).then((response) {
-        return BookListEntry.listFromJson(
-            response);
+      return BookListEntry.listFromJson(response);
     });
   }
 
-  Future editBookListEntry(int bookId, int rating, String status) {
-    BookListEdit bookListEdit = new BookListEdit(bookId, rating, status);
+  Future editBookListEntry(int bookId, int rating, Status status) {
+    BookListEdit bookListEdit =
+        new BookListEdit(bookId, rating, describeEnum(status));
     return apiService.post(ApiEndpoints.bookList, bookListEdit);
   }
 
   Future<Response> deleteBookListEntry(int bookId) {
-    return apiService
-        .delete('${ApiEndpoints.bookList}/$bookId');
+    return apiService.delete('${ApiEndpoints.bookList}/$bookId');
   }
 }
--- lib/model/Status.dart
+++ lib/model/Status.dart
@@ -1,5 +1,28 @@
+import 'package:flutter/foundation.dart';
+
 enum Status {
   COMPLETED,
   READING,
   PLAN_TO_READ
+}
+
+Status statusFromString(String str) {
+  return Status.values.firstWhere((status) => describeEnum(status) == str);
+}
+
+String statusToPrettyString(Status status) {
+  switch (status) {
+    case Status.COMPLETED: {
+      return 'Completed';
+    }
+    case Status.READING: {
+      return 'Reading';
+    }
+    case Status.PLAN_TO_READ: {
+      return 'Plan to Read';
+    }
+    default: {
+      return describeEnum(status);
+    }
+  }
 }
